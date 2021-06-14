2a92529b4de0e3ba429bdcb687df53031202aac4
Abid Hasan <38142060+AbidHasan007@users.noreply.github.com>
2021-06-14 19:19:41 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text ='Learning by doing...';
+let i=1;
+let speed = 300  / speedEl.value;
+speedEl.addEventListener('input', (e)=> speed = 300 / e.target.value);
+writeText()
+function writeText(){   
+    textEl.innerHTML =` ${text.slice( 0, i)}_`;
+    i++    
+    if(i > text.length){
+        i=1
+    }
+    setTimeout(writeText,speed)
+}
